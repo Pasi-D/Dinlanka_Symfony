fcaf9283d9e10480febe113aad5e96e06f92efea
Pasindu Dilshan <pasindudil1995@gmail.com>
2018-04-22 20:20:40 +0530

    clearing cache before AWS EB deployment

--- var/cache/prod/appProdProjectContainerUrlMatcher.php
+++ var/cache/prod/appProdProjectContainerUrlMatcher.php
@@ -28,80 +28,82 @@ class appProdProjectContainerUrlMatcher extends Symfony\Bundle\FrameworkBundle\R
             $canonicalMethod = 'GET';
         }
 
-        // homepage
+        // indexpage
         if ('' === $trimmedPathinfo) {
-            $ret = array (  '_controller' => 'AppBundle\\Controller\\DefaultController::indexAction',  '_route' => 'homepage',);
+            $ret = array (  '_controller' => 'AppBundle\\Controller\\DinLankaController::indexAction',  '_route' => 'indexpage',);
             if ('/' === substr($pathinfo, -1)) {
                 // no-op
             } elseif ('GET' !== $canonicalMethod) {
-                goto not_homepage;
+                goto not_indexpage;
             } else {
-                return array_replace($ret, $this->redirect($rawPathinfo.'/', 'homepage'));
+                return array_replace($ret, $this->redirect($rawPathinfo.'/', 'indexpage'));
             }
 
             return $ret;
         }
-        not_homepage:
+        not_indexpage:
 
-        if (0 === strpos($pathinfo, '/dinlanka')) {
-            // indexpage
-            if ('/dinlanka' === $pathinfo) {
-                return array (  '_controller' => 'AppBundle\\Controller\\DinLankaController::indexAction',  '_route' => 'indexpage',);
+        // about
+        if ('/about' === $pathinfo) {
+            return array (  '_controller' => 'AppBundle\\Controller\\DinLankaController::aboutAction',  '_route' => 'about',);
+        }
+
+        // air_freight
+        if ('/air-freight' === $pathinfo) {
+            return array (  '_controller' => 'AppBundle\\Controller\\DinLankaController::airfreightAction',  '_route' => 'air_freight',);
+        }
+
+        // contact
+        if ('/contact' === $pathinfo) {
+            return array (  '_controller' => 'AppBundle\\Controller\\DinLankaController::contactAction',  '_route' => 'contact',);
+        }
+
+        // cargo_insurance
+        if ('/dinlanka-cargo-insurance' === $pathinfo) {
+            return array (  '_controller' => 'AppBundle\\Controller\\DinLankaController::cargoAction',  '_route' => 'cargo_insurance',);
+        }
+
+        // ior & eor
+        if ('/ior-eor-services' === $pathinfo) {
+            return array (  '_controller' => 'AppBundle\\Controller\\DinLankaController::ioreorAction',  '_route' => 'ior & eor',);
+        }
+
+        // containers for sale
+        if ('/new-shipping-containers-for-sale' === $pathinfo) {
+            return array (  '_controller' => 'AppBundle\\Controller\\DinLankaController::containers_for_saleAction',  '_route' => 'containers for sale',);
+        }
+
+        // ocean freight
+        if ('/ocean-freight' === $pathinfo) {
+            return array (  '_controller' => 'AppBundle\\Controller\\DinLankaController::oceanfreightAction',  '_route' => 'ocean freight',);
+        }
+
+        if (0 === strpos($pathinfo, '/s')) {
+            // sea-air-service
+            if ('/sea-air-service' === $pathinfo) {
+                return array (  '_controller' => 'AppBundle\\Controller\\DinLankaController::sea_air_servicesAction',  '_route' => 'sea-air-service',);
             }
 
-            // about
-            if ('/dinlanka/about' === $pathinfo) {
-                return array (  '_controller' => 'AppBundle\\Controller\\DinLankaController::aboutAction',  '_route' => 'about',);
+            // service
+            if ('/service' === $pathinfo) {
+                return array (  '_controller' => 'AppBundle\\Controller\\DinLankaController::serviceAction',  '_route' => 'service',);
             }
 
-            // air_freight
-            if ('/dinlanka/air-freight' === $pathinfo) {
-                return array (  '_controller' => 'AppBundle\\Controller\\DinLankaController::airfreightAction',  '_route' => 'air_freight',);
+            // supply-chain-manegment
+            if ('/supply-chain-manegment' === $pathinfo) {
+                return array (  '_controller' => 'AppBundle\\Controller\\DinLankaController::supply_chain_managementAction',  '_route' => 'supply-chain-manegment',);
             }
 
-            // contact
-            if ('/dinlanka/contact' === $pathinfo) {
-                return array (  '_controller' => 'AppBundle\\Controller\\DinLankaController::contactAction',  '_route' => 'contact',);
-            }
+        }
 
-            // cargo_insurance
-            if ('/dinlanka/dinlanka-cargo-insurance' === $pathinfo) {
-                return array (  '_controller' => 'AppBundle\\Controller\\DinLankaController::cargoAction',  '_route' => 'cargo_insurance',);
-            }
+        // freezer transportation & solutions
+        if ('/freezer-transportation-solutions' === $pathinfo) {
+            return array (  '_controller' => 'AppBundle\\Controller\\DinLankaController::freezer_transportationAction',  '_route' => 'freezer transportation & solutions',);
+        }
 
-            // ior & eor
-            if ('/dinlanka/ior-eor-services' === $pathinfo) {
-                return array (  '_controller' => 'AppBundle\\Controller\\DinLankaController::ioreorAction',  '_route' => 'ior & eor',);
-            }
-
-            // containers for sale
-            if ('/dinlanka/new-shipping-containers-for-sale' === $pathinfo) {
-                return array (  '_controller' => 'AppBundle\\Controller\\DinLankaController::containers_for_saleAction',  '_route' => 'containers for sale',);
-            }
-
-            // ocean freight
-            if ('/dinlanka/ocean-freight' === $pathinfo) {
-                return array (  '_controller' => 'AppBundle\\Controller\\DinLankaController::oceanfreightAction',  '_route' => 'ocean freight',);
-            }
-
-            if (0 === strpos($pathinfo, '/dinlanka/s')) {
-                // sea-air-service
-                if ('/dinlanka/sea-air-service' === $pathinfo) {
-                    return array (  '_controller' => 'AppBundle\\Controller\\DinLankaController::sea_air_servicesAction',  '_route' => 'sea-air-service',);
-                }
-
-                // service
-                if ('/dinlanka/service' === $pathinfo) {
-                    return array (  '_controller' => 'AppBundle\\Controller\\DinLankaController::serviceAction',  '_route' => 'service',);
-                }
-
-                // supply-chain-manegment
-                if ('/dinlanka/supply-chain-manegment' === $pathinfo) {
-                    return array (  '_controller' => 'AppBundle\\Controller\\DinLankaController::supply_chain_managementAction',  '_route' => 'supply-chain-manegment',);
-                }
-
-            }
-
+        // entrepot trade
+        if ('/entrepot-trade' === $pathinfo) {
+            return array (  '_controller' => 'AppBundle\\Controller\\DinLankaController::entrepot_tradeAction',  '_route' => 'entrepot trade',);
         }
 
         if ('/' === $pathinfo && !$allow) {
