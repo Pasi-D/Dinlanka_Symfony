6cd236db21410fa0c4c3bbe21dbc6b74a7e48222
DinukaGanepola <nuwanraseeganepola@gmail.com>
2018-04-25 11:05:52 +0530

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,50 @@
-//webpack.config.js
 var Encore = require('@symfony/webpack-encore');
 
 Encore
-// the project directory where all compiled assets will be stored
+    // the project directory where all compiled assets will be stored
     .setOutputPath('web/build/')
 
     // the public path used by the web server to access the previous directory
     .setPublicPath('/build')
 
-    // will create web/build/app.js and web/build/app.css
-    .addEntry('style', './web/js/')
+    // will create public/build/index.js and public/build/app.css
+    .addEntry('index', './web/js/vendor/modernizr-2.8.1.min.js')
+    .addEntry('html5shim', './web/js/vendor/html5shim.js')
+    .addEntry('respond.min', './web/js/vendor/respond.min.js')
+    .addEntry('bootstrap.min', './web/js/bootstrap.min.js')
+    .addEntry('hippo-offcanvas', './web/js/hippo-offcanvas.js')
+    .addEntry('jquery.magnific-popup.min', './web/js/jquery.magnific-popup.min.js')
+    .addEntry('jquery.stellar', './web/js/jquery.stellar.js')
+    .addEntry('jquery.countTo', './web/js/jquery.countTo.js')
+    .addEntry('classie', './web/js/classie.js')
+    .addEntry('selectFx', './web/js/selectFx.js')
+    .addEntry('jquery.sticky-kit.min', './web/js/jquery.sticky-kit.min.js')
+    .addEntry('twitterFetcher_min', './web/js/twitterFetcher_min.js')
+    .addEntry('scripts', './web/js/scripts.js')
 
-// create hashed filenames (e.g. app.abc123.css)
-// .enableVersioning()
 
-// allow sass/scss files to be processed
-// .enableSassLoader()
+
+
+
+
+    // allow legacy applications.js')
+//      to use $/jQuery as a global variable
+   // .autoProvidejQuery()
+
+    // enable source maps during development
+    //.enableSourceMaps(!Encore.isProduction())
+
+    // empty the outputPath dir before each build
+    //.cleanupOutputBeforeBuild()
+
+    // show OS notifications when builds finish/fail
+    //.enableBuildNotifications()
+
+    // create hashed filenames (e.g. app.abc123.csss)
+    // .enableVersioning()
+
+    // allow sass/scss files to be processed
+    // .enableSassLoader()
 ;
 
 // export the final configuration
