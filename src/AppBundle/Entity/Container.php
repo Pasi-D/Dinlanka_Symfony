1027c26b19453fcdc36a0545f6f8c8a2f70ef958
Kumudini <kkperera14@gmail.com>
2018-05-15 21:37:18 +0530

    database

---
+++ src/AppBundle/Entity/Container.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Container
+ *
+ * @ORM\Table(name="container")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ContainerRepository")
+ */
+class Container
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="model_id", type="string", length=255, unique=true)
+     */
+    private $modelId;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="model_name", type="string", length=255, unique=true)
+     */
+    private $modelName;
+
+    /**
+     * @var string
+     *
+     * @Assert\Currency
+     * @ORM\Column(name="price", type="string", length=255)
+     */
+    private $price;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set modelId
+     *
+     * @param string $modelId
+     *
+     * @return Container
+     */
+    public function setModelId($modelId)
+    {
+        $this->modelId = $modelId;
+
+        return $this;
+    }
+
+    /**
+     * Get modelId
+     *
+     * @return string
+     */
+    public function getModelId()
+    {
+        return $this->modelId;
+    }
+
+    /**
+     * Set modelName
+     *
+     * @param string $modelName
+     *
+     * @return Container
+     */
+    public function setModelName($modelName)
+    {
+        $this->modelName = $modelName;
+
+        return $this;
+    }
+
+    /**
+     * Get modelName
+     *
+     * @return string
+     */
+    public function getModelName()
+    {
+        return $this->modelName;
+    }
+
+    /**
+     * Set price
+     *
+     * @param string $price
+     *
+     * @return Container
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get price
+     *
+     * @return string
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+}
+
---
+++ src/AppBundle/Entity/Login.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Login
+ *
+ * @ORM\Table(name="login")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\LoginRepository")
+ */
+class Login
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="user_name", type="string", length=255)
+     */
+    private $userName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @ORM\OneToOne(targetEntity="user", inversedBy="login")
+     * @ORM\JoinColumn(name="user_id", referencedColumnName="id")
+     */
+    private $user;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set userName
+     *
+     * @param string $userName
+     *
+     * @return Login
+     */
+    public function setUserName($userName)
+    {
+        $this->userName = $userName;
+
+        return $this;
+    }
+
+    /**
+     * Get userName
+     *
+     * @return string
+     */
+    public function getUserName()
+    {
+        return $this->userName;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     *
+     * @return Login
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+}
+
