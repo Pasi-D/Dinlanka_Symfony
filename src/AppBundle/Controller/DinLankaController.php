5c09956b0ca06af547d7e282cadedcbb96e67797
Kumudini <kkperera14@gmail.com>
2018-05-17 20:00:29 +0530

    quatation controller

--- src/AppBundle/Controller/DinLankaController.php
+++ src/AppBundle/Controller/DinLankaController.php
@@ -3,13 +3,16 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\HttpFoundation\Session\Flash\FlashBag;
+
 use AppBundle\Entity\Contact;
+
 class DinLankaController extends Controller
 {
     /**
