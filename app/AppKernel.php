967c62eeedcf7f58f3eda91cc1d4968913342ef1
Kumudini <kkperera14@gmail.com>
2018-06-08 17:09:09 +0530

    conflicts

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,12 +16,12 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
+
             new \Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
-            
             new EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle(),
 
             new AppBundle\AppBundle(),
+
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
