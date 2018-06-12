35edb90dd96b9e96751438bd18d56c6367d49dde
Pasindu Dilshan <pasindudil1995@gmail.com>
2018-06-12 14:49:42 +0530

    ContactUsController + Quotation Controller

--- src/AppBundle/Controller/QuotationController.php
+++ src/AppBundle/Controller/QuotationController.php
@@ -1,33 +1,47 @@
 <?php
 namespace AppBundle\Controller;
-
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\HttpFoundation\Session\Flash\FlashBag;
-//use AppBundle\Entity\Contact;
-
+use AppBundle\Entity\Quotation;
+class QuotationController extends Controller
+{
     /**
      * @Route("/quote", name="quotation")
      */
     public function quotetAction(Request $request)
     {
-      $contact = new Contact;     
+      $quote = new Quotation;     
  
      # Add form fields
-       $form = $this->createFormBuilder($contact)
+       $form = $this->createFormBuilder($quote)
        ->add('Name', TextType::class, array('label'=> 'Name', 'attr' => array('class' => 'form-control', 'style' => 'margin-bottom:7px')))
-       ->add('Email_address', EmailType::class, array('label'=> 'Email address','attr' => array('class' => 'form-control', 'style' => 'margin-bottom:7px')))
-       ->add('Subject', TextType::class, array('label'=> 'Subject','attr' => array('class' => 'form-control', 'style' => 'margin-bottom:6px')))
-       ->add('Phone_Number', NumberType::class, array('label'=> 'Phone Number','attr' => array('class' => 'form-control', 'style' => 'margin-bottom:6px')))
-       ->add('Your_Message', TextareaType::class, array('label'=> 'Your Message','attr' => array('class' => 'form-control', 'style' => 'margin-bottom:7px')))
-
+       ->add('Company_Name', TextType::class, array('label'=> 'Company Name', 'attr' => array('class' => 'form-control', 'style' => 'margin-bottom:7px')))
+       ->add('Email', EmailType::class, array('label'=> 'Your Email Address','attr' => array('class' => 'form-control', 'style' => 'margin-bottom:7px')))
+       ->add('Contact_Number', NumberType::class, array('label'=> 'Contact Number','attr' => array('class' => 'form-control', 'style' => 'margin-bottom:6px')))
+       ->add('Origin', ChoiceType::class, [
+                                              'choices' => [
+                                                            new Quotation('Ocean Freight'),
+                                                            new Quotation('Cat2'),
+                                                            new Quotation('Cat3'),
+                                                            new Quotation('Cat4'),
+                                                                                ],
+                                              'choice_label' => function($quote, $key, $index) {
+                                                    /** @var Quotation $quote */
+                                                    return strtoupper($quote->getName());
+                                                },
+                                              'choice_attr' => function($quote, $key, $index) {
+                                                    return ['class' => 'quote_'.strtolower($quote->getName())];
+                                                },
+                                              ])
+        //Origin
+       //Transportation_Mode
        ->getForm();
      # Handle form response
        $form->handleRequest($request);
@@ -35,28 +49,27 @@ use Symfony\Component\HttpFoundation\Session\Flash\FlashBag;
  
        if($form->isSubmitted() &&  $form->isValid()){
            $name = $form['Name']->getData();
-           $emailAddress = $form['Email_address']->getData();
-           $subject = $form['Subject']->getData();
-           $phoneNumber = $form['Phone_Number']->getData();
-           $yourMessage = $form['Your_Message']->getData(); 
+           $companyName = $form['Company_Name']->getData();
+           $email = $form['Email']->getData();
+           $$contactNumber = $form['Contact_Number']->getData();
         
      # set form data   
  
-           $contact->setName($name);
-           $contact->setEmailAddress($emailAddress);          
-           $contact->setSubject($subject);
-           $contact->setPhoneNumber($phoneNumber);     
-           $contact->setYourMessage($yourMessage);                
+           $quote->setName($name);
+           $quote->setEmailAddress($emailAddress);          
+           $quote->setSubject($subject);
+               
     
       # finally add data in database
  
            $entityManager = $this->getDoctrine()->getManager();      
-           $entityManager -> persist($contact);
+           $entityManager -> persist($quote);
            $entityManager -> flush(); 
             $this->addFlash('Success', 'Your Message has Sent!');
-           return $this->redirectToRoute('contact');
+           return $this->redirectToRoute('quotation');
        }     
-        return $this->render('dinlanka/contact.html.twig',
+        return $this->render('dinlanka/quotation.html.twig',
             array('form' => $form->createView())
         );  
     }
+  }
