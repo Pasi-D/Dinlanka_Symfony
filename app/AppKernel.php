46503d563e48bdee0f267508fa1956b1ef3e2094
kavinda-seasath <seasath@outlook.com>
2018-06-14 17:14:16 +0530

    activate the FOSuser bundle and add Fosuser entity

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
 
             new \Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
 
             new AppBundle\AppBundle(),
 
