cbc17fa14c89f9d8fe9dc417c08925131b3aada4
xXZang3tsuXx <pasindudil1995@gmail.com>
2018-08-23 07:54:36 +0530

    Twilio messaging via admin panel

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -3,6 +3,9 @@
 namespace AppBundle\Controller;
 
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AdminController as BaseAdminController;
+use Symfony\Component\PropertyAccess\PropertyAccess;
+
+use Twilio\Rest\Client;
 
 class AdminController extends BaseAdminController
 
@@ -35,7 +38,92 @@ class AdminController extends BaseAdminController
     #message sending capability - via twilio
     public function messageAction()
     {
-        return $this->redirectToRoute('indexpage');
+        $propertyAccessor = PropertyAccess::createPropertyAccessor();
+
+        $id = $this->request->query->get('id');
+
+        $freight = $this->getDoctrine()->getRepository('AppBundle:AirFreight')->findBy(
+            array(
+                'id' => $id
+            )
+        );
+
+        //get the contact number from description and other details
+        $cargo_id = $propertyAccessor->getValue($freight, '[0].airFreightBillNumber');
+
+        $description = $propertyAccessor->getValue($freight, '[0].description');
+
+        $mobile_number = explode("]", explode(": ", explode(" || ", $description)[1])[1])[0];
+
+        $customer_name = explode("]", explode(": ", explode(" || ", $description)[0])[1])[0];
+
+        $last_location = $propertyAccessor->getValue($freight, '[0].currentLocation');
+
+
+
+
+        // Your Account SID and Auth Token from twilio.com/console
+        $account_sid = 'AC42d977276b610278f29541e5d72cf344';
+        $auth_token = '5b7c0c04ce5cd95a6c175eff51c3b80a';
+
+        // A Twilio number you own with SMS capabilities
+        $twilio_number = "+16175397943";
+
+        $client = new Client($account_sid, $auth_token);
+
+        $customer_caller_id = explode("+", $mobile_number)[1];
+        //get all listed callerIds in twilio
+        $outgoingCallerIds = $client->outgoingCallerIds
+            ->read();
+
+        $twilio_validate = false;
+        foreach ($outgoingCallerIds as $record){
+            if ($record->friendlyName === $customer_caller_id){
+                //set the flag to true
+                $twilio_validate = true;
+            }
+        }
+
+        if ($twilio_validate){
+            //send the sms without validation
+            $client->messages->create(
+            // Where to send a text message (your cell phone?)
+                $mobile_number,
+                array(
+                    'from' => $twilio_number,
+                    'body' => 'Hey '.$customer_name.' your shipment with cargo number : '.$cargo_id.' is now at '.$last_location
+                )
+            );
+
+            var_export($customer_caller_id.' number is already validated');
+            return $this->render('dinlanka/index.html.twig');
+        }else{
+            //validate as an outgoing caller id first
+            $client->validationRequests
+                ->create($mobile_number, // phoneNumber
+                    array(
+                        "friendlyName" => $customer_caller_id
+                    )
+                );
+
+            //send the sms after validation
+            $client->messages->create(
+            // Where to send a text message (your cell phone?)
+                $mobile_number,
+                array(
+                    'from' => $twilio_number,
+                    'body' => 'Hey '.$customer_name.' your shipment with cargo number : '.$cargo_id.' is now at '.$last_location
+                )
+            );
+            var_export($customer_caller_id.' number was not validated before');
+            return $this->render('dinlanka/index.html.twig');
+        }
+
+
+
+
+
+
     }
 
 }
